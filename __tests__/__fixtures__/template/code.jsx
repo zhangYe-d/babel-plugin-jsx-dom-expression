7cc0a408ef9c8c5c10de65d6ab5340c44c6aa275
zhangYe <daizhichun621@gmail.com>
2024-12-19 03:32:22 +0800

    add some tests concerning template

--- __tests__/__fixtures__/template/code.jsx
+++ __tests__/__fixtures__/template/code.jsx
@@ -1,4 +1,17 @@
 const text = "hello world";
+let bool = true;
 
 const jsx = <h1>hello world</h1>;
 const jsx2 = <h1>{text}</h1>;
+const jsx5 = <h1>{bool ? "bool" : text}</h1>;
+
+const jsx3 = (
+  <h1>
+    <div>happy day</div>
+  </h1>
+);
+const jsx4 = (
+  <h1>
+    <div>{text}</div>
+  </h1>
+);
--- __tests__/__fixtures__/template/output.js
+++ __tests__/__fixtures__/template/output.js
@@ -1,10 +1,26 @@
 import { insert as _insert, template as _template } from "../../mockModule";
 var _tmp$ = _template(`<h1>hello world</h1>`),
-  _tmp$2 = _template(`<h1></h1>`);
+  _tmp$2 = _template(`<h1></h1>`),
+  _tmp$3 = _template(`<h1><div>happy day</div></h1>`),
+  _tmp$4 = _template(`<h1><div></div></h1>`);
 const text = "hello world";
+let bool = true;
 const jsx = _tmp$();
 const jsx2 = (() => {
   var _$el2 = _tmp$2();
   _insert(_$el2, text);
   return _$el2;
+})();
+const jsx5 = (() => {
+  var _$el3 = _tmp$2();
+  _insert(_$el3, bool ? "bool" : text);
+  return _$el3;
+})();
+const jsx3 = _tmp$3();
+const jsx4 = (() => {
+  var _$el5 = _tmp$4(),
+    _$el6 = _$el5.firstChild,
+    _$el7 = _$el6.nextSibling;
+  _insert(_$el7, text);
+  return _$el5;
 })();
